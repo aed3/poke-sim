fe5ae85f862ed2bf0139fd51ab815b9a55d6b023
aed3 <aed@thethirdbuild.com>
2023-04-11 20:50:56 -0700

    Updated src to reflect new direction (#10)
    * Updated src to reflect new direction
    - Removed stand-in classes
    - Moved enums to Dex
    - Updated single header file creator to include external libraries
    - Removed Name.hpp action exceptions
    
    * Change to make past commit work
    
    * Fixed ORB comments and replaced uint with size_t
    
    * Update workflow formatting (build shouldn't rerun)
    
    * Updating Single Header File
    
    * Forgot Windows requires join for path
    The single header file was including Catch2 code because it was assuming '/' was the path separator rather than '\'
    
    * Fix last ORB issue
    
    * Updating Single Header File
    
    ---------
    
    Co-authored-by: Milo[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Utils/Universal.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace pokesim::internal {
-template <typename First, typename... T>
-bool matchesFirst(const First& first, const T&... rest) {
-  static_assert(std::conjunction_v<std::is_same<First, T>...>);
-  return ((first == rest) || ...);
-}
-}  // namespace pokesim::internal
