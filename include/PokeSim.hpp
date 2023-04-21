8429e5d8ed33cc1ef69c1a4bd7e49af4c6c3792c
aed3 <32887801+aed3@users.noreply.github.com>
2023-04-20 21:08:07 -0700

    Breaking stats into 6 components

--- include/PokeSim.hpp
+++ include/PokeSim.hpp
@@ -3677,16 +3677,31 @@ struct SpeedSort {
 
 #include <cstdint>
 
-namespace pokesim {
-struct Stats {
+namespace pokesim::stat {
+struct HP {
   std::uint16_t hp;
+};
+
+struct Atk {
   std::uint16_t atk;
+};
+
+struct Def {
   std::uint16_t def;
+};
+
+struct Spa {
   std::uint16_t spa;
+};
+
+struct Spd {
   std::uint16_t spd;
+};
+
+struct Spe {
   std::uint16_t spe;
 };
-}  // namespace pokesim
+}  // namespace pokesim::stat
 
 /////////////////////// END OF src/Components/Stats.hpp ////////////////////////
 
