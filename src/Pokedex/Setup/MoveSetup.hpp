225f65a9026de24a95e7aa30ef5ce33c51c3041f
aed3 <aed@thethirdbuild.com>
2023-05-22 20:52:07 -0700

    Fix MoveEffectSetup::setBoost for BoostType

--- src/Pokedex/Setup/MoveSetup.hpp
+++ src/Pokedex/Setup/MoveSetup.hpp
@@ -41,7 +41,7 @@ struct MoveEffectSetup : DexDataSetup {
       std::is_same<AtkBoost, BoostType>() || std::is_same<DefBoost, BoostType>() ||
       std::is_same<SpaBoost, BoostType>() || std::is_same<SpdBoost, BoostType>() ||
       std::is_same<SpeBoost, BoostType>());
-    handle.emplace<SpdBoost>(boost);
+    handle.emplace<BoostType>(boost);
   }
 };
 }  // namespace pokesim::dex::internal
