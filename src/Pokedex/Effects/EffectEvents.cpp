3c831e1dd264e259ea78e43264d77966f3c38796
aed3 <32887801+aed3@users.noreply.github.com>
2025-05-18 17:43:39 -0500

    PR Windows build fixes

--- src/Pokedex/Effects/EffectEvents.cpp
+++ src/Pokedex/Effects/EffectEvents.cpp
@@ -10,7 +10,7 @@ namespace pokesim::dex {
 void internal::ChoiceLockEvents::onDisableMove(
   types::registry& registry, const pokesim::ChoiceLock& choiceLocked, const MoveSlots& moveSlots) {
   POKESIM_REQUIRE(
-    std::find(moveSlots.val.begin(), moveSlots.val.end(), choiceLocked.val),
+    std::find(moveSlots.val.begin(), moveSlots.val.end(), choiceLocked.val) != moveSlots.val.end(),
     "Should skip if the move is no longer present, but when does that happen?");
 
   for (types::entity entity : moveSlots.val) {
