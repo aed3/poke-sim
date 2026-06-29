c02363aaf44adf052bafe6343c7342c9aa3714b1
aed3 <32887801+aed3@users.noreply.github.com>
2026-06-29 18:19:27 -0500

    Update PokeSim.cpp

--- extras/PokeSim.cpp
+++ extras/PokeSim.cpp
@@ -1768,8 +1768,8 @@ void createInitialBattle(
   battleSetup.setSide(p1Entity);
   battleSetup.setSide(p2Entity);
 
-  p1SideSetup.setOpponent(p2Entity, battleInfo.sides.p2().team.size());
-  p2SideSetup.setOpponent(p1Entity, battleInfo.sides.p1().team.size());
+  p1SideSetup.setOpponent(p2Entity, (types::teamPositionIndex)battleInfo.sides.p2().team.size());
+  p2SideSetup.setOpponent(p1Entity, (types::teamPositionIndex)battleInfo.sides.p1().team.size());
 
   p1SideSetup.setBattle(battleEntity);
   p2SideSetup.setBattle(battleEntity);
